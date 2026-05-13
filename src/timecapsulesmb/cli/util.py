baec61827abd65558e91764a7f5a124b1f4fedc6
James Chang <jamesc@berkeley.edu>
2026-05-12 23:31:00 -0700

    Update util.py

--- src/timecapsulesmb/cli/util.py
+++ src/timecapsulesmb/cli/util.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 NETBSD4_REBOOT_GUIDANCE = "NetBSD 4 devices cannot auto-run Samba after a reboot."
 NETBSD4_REBOOT_FOLLOWUP = "Run `activate` after a reboot if the device did not auto-start Samba."
 # Update this version info for each release, including beta releases
-CLI_VERSION = "2.1.0-rc2b"
-RELEASE_TAG = "v2.1.0-rc2b"
-CLI_VERSION_CODE = 20112
+CLI_VERSION = "2.1.0-rc2c"
+RELEASE_TAG = "v2.1.0-rc2c"
+CLI_VERSION_CODE = 20113
 SAMBA_VERSION = "4.24.1"
 
 ANSI_RED = "\033[31m"
