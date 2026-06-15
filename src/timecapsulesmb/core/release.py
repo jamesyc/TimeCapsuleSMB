45da08847e6314e094238f5a26d4ca4f94951089
James Chang <jamesc@berkeley.edu>
2026-06-15 04:25:36 -0700

    Update to v2.2.5

--- src/timecapsulesmb/core/release.py
+++ src/timecapsulesmb/core/release.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 # Update this version info for each release, including beta releases.
-CLI_VERSION = "2.2.4"
-RELEASE_TAG = "v2.2.4"
-CLI_VERSION_CODE = 20210
+CLI_VERSION = "2.2.5"
+RELEASE_TAG = "v2.2.5"
+CLI_VERSION_CODE = 20211
 SAMBA_VERSION = "4.24.3"
