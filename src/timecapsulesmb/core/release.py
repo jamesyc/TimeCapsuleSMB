f865c654421395664ee14ab0af976c8b6b7ca8b2
James Chang <jamesc@berkeley.edu>
2026-06-19 02:14:02 -0700

    Bump version to 2.2.7

--- src/timecapsulesmb/core/release.py
+++ src/timecapsulesmb/core/release.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 # Update this version info for each release, including beta releases.
-CLI_VERSION = "2.2.6"
-RELEASE_TAG = "v2.2.6"
-CLI_VERSION_CODE = 20212
+CLI_VERSION = "2.2.7"
+RELEASE_TAG = "v2.2.7"
+CLI_VERSION_CODE = 20213
 SAMBA_VERSION = "4.24.3"
